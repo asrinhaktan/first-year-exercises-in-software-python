f8e1deb4fa8e3795eed573b779c4ed314132d993
Asrın Haktan <56225086+StillCold0@users.noreply.github.com>
2020-09-15 02:10:17 +0300

    Add files via upload

---
+++ fingertest.py
@@ -0,0 +1,21 @@
+import time
+import datetime
+print("Please type your text after 3 seconds")
+
+print("3")
+time.sleep(1)
+print("2")
+time.sleep(1)
+print("Go!")
+time.sleep(0.2)
+before = datetime.datetime.now()
+
+text=input("Type here:")
+after = datetime.datetime.now()
+
+speed = after - before
+seconds = round(speed.total_seconds(),2)
+letter_per_second = round(len(text) / seconds,1)
+
+print("You typed in : {} seconds.".format(seconds))
+print("{} letters per second.".format(letter_per_second))
